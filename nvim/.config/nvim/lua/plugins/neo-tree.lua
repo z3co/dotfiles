88597f940616ad88802ca0600bb898367e2090cb
Jeppe Wolff Johansen <Jepper123411@proton.me>
2025-05-18 18:45:49 +0200

    Updated neotree to be thinner

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -8,5 +8,42 @@ return {
   config = function()
     vim.keymap.set('n', '<leader>e', ':Neotree filesystem focus<CR>', {})
     vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle<CR>', {})
+    require("neo-tree").setup({
+      enable_diagnostics = false,
+      window = {
+        width = 30,
+      },
+      git_status = {
+        git_status = {
+            symbols = {
+              -- Change type
+              added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+              modified = " ", -- or "", but this is redundant info if you use git_status_colors on the name
+              deleted = "✖", -- this can only be used in the git_status source
+              renamed = "󰁕", -- this can only be used in the git_status source
+              -- Status type
+              untracked = "",
+              ignored = "",
+              unstaged = "󰄱",
+              staged = "",
+              conflict = "",
+            },
+          },
+      },
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            "node_modules",
+            ".git",
+            ".next",
+            "next-env.d.ts",
+            "tsconfig.tsbuildinfo",
+            ".npmrc"
+          }
+        }
+      }
+    })
   end
 }
